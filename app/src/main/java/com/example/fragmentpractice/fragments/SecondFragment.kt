d6f4ef6323befe1c186f5dc3b5232835a14b14aa
mykim76 <iyoungfm@gmail.com>
2020-06-04 21:52:14 +0900

    프래그먼트 2개 추가 => 세번째 프래그는 1번째 프래그먼트를 재사용

---
+++ app/src/main/java/com/example/fragmentpractice/fragments/SecondFragment.kt
@@ -0,0 +1,25 @@
+package com.example.fragmentpractice.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.fragmentpractice.R
+import kotlinx.android.synthetic.main.fragment_second.*
+
+class SecondFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_second,container,false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        txtMessage2.text="또 안녕하세요"
+    }
+}
