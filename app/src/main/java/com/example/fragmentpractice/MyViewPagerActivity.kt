fbb2e7178cab4b53536ea13b50e0a915ca8bb51c
mykim76 <iyoungfm@gmail.com>
2020-06-04 22:01:03 +0900

    빈 액티비티를 시작으로 변경

---
+++ app/src/main/java/com/example/fragmentpractice/MyViewPagerActivity.kt
@@ -0,0 +1,24 @@
+package com.example.fragmentpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyViewPagerActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_view_pager)
+
+        setValues()
+        setupEvents()
+    }
+
+    override fun setValues() {
+
+
+    }
+
+    override fun setupEvents() {
+        TODO("Not yet implemented")
+    }
+}
