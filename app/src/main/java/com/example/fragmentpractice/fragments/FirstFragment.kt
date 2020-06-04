c9470b859ff3a8d9c4764506a63f4808c3defc3e
mykim76 <iyoungfm@gmail.com>
2020-06-04 21:19:41 +0900

    onActivityCreated에 코드를 작성했으나 프래그먼트가 화면에 나타나지 않음. ㅠㅠ

---
+++ app/src/main/java/com/example/fragmentpractice/fragments/FirstFragment.kt
@@ -0,0 +1,26 @@
+package com.example.fragmentpractice.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.fragmentpractice.R
+import kotlinx.android.synthetic.main.fragment_first.*
+
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first,container,false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        txtMessage.text="안녕하세요"
+    }
+}
--- app/src/main/java/com/example/fragmentpractice/fragments/firstFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.example.fragmentpractice.fragments
-
-import androidx.fragment.app.Fragment
-
-class firstFragment : Fragment() {
-}
